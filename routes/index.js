51b654bf9a842f189656cd3928a7fca5e7cc53e2
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-04-12 00:34:27 +0300

    create empty movies route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const userRouter = require('./users');
+const movieRouter = require('./movies');
 
 module.exports = router;
