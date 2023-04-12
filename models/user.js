dafc8c32cb9774bc6da57e6c79341d00b4dd5f45
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-04-12 16:46:35 +0300

    create user email paswword check for login

--- models/user.js
+++ models/user.js
@@ -24,4 +24,23 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+//проверка совпадения полей email и password при авторизации пользователя
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new AuthError('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new AuthError('Неправильные почта или пароль'));
+        }
+
+        return user;
+      });
+    });
+};
+
 module.exports = mongoose.model('user', userSchema);
