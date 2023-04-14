348e4380190d5a4394a45dd214263bc4757dc7e2
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-04-14 22:23:59 +0300

    create custom joi validation messages

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,26 +1,38 @@
+//импорты
 const { isCelebrateError } = require("celebrate");
 const { ERROR_CODE_400, ERROR_CODE_500 } = require("../config");
 
+//////////////////////////////////////////////////////////////////////////////////////
+
 // централизованный обработчик ошибок
 const errorHandler = (err, req, res, next) => {
+  //достём статус ошибки (если его нет, задаём по умолчанию) и достаём message
   const { statusCode = ERROR_CODE_500, message } = err;
 
   //проверям ошибку на принадлежность к celebrate
   if (isCelebrateError(err)) {
+    // if (err.details) {
     //достём details из Map()
-    const errorBody = err.details.get("body");
+    const errorBody = err.details.get("body") || err.details.get("params");
     //достаём массив из details
     const {
       details: [errorDetails],
     } = errorBody;
+
     //возвращаем только сообщение об ошибке
     return res.status(ERROR_CODE_400).send({ message: errorDetails.message });
+    // }
+    // next(err);
   }
 
+  //проверям статус ошибки и отправлеям соответсвующее ему сообщение
   res.status(statusCode).send({
     message:
       statusCode === ERROR_CODE_500 ? "На сервере произошла ошибка." : message,
   });
 };
 
+//////////////////////////////////////////////////////////////////////////////////////
+
+//экспорт
 module.exports = { errorHandler };
---
+++ middlewares/movieInputValidator.js
@@ -0,0 +1,113 @@
+//импорты
+const { celebrate, Joi } = require("celebrate");
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+//объект с ключами для валидации инпутов фильма
+const movieValKeys = {
+  country: Joi.string().required().messages({
+    "string.base": `Поле {#label} должно быть строкой`,
+    "string.empty": `Поле {#label} не может быть пустым`,
+    "any.required": `Поле {#label} обязательно для заполнения`,
+  }),
+  director: Joi.string().required().messages({
+    "string.email": "Поле {#label} должно быть валидным имеил адресом",
+    "string.empty": `Поле {#label} не может быть пустым`,
+    "any.required": `Поле {#label} обязательно для заполнения`,
+  }),
+  duration: Joi.number().required().messages({
+    "number.base": `Поле {#label} должно быть числом`,
+    "number.empty": `Поле {#label} не может быть пустым`,
+    "any.required": `Поле {#label} обязательно для заполнения`,
+  }),
+  year: Joi.string().required().messages({
+    "string.base": `Поле {#label} должно быть строкой`,
+    "string.empty": `Поле {#label} не может быть пустым`,
+    "any.required": `Поле {#label} обязательно для заполнения`,
+  }),
+  description: Joi.string().required().messages({
+    "string.base": `Поле {#label} должно быть строкой`,
+    "string.empty": `Поле {#label} не может быть пустым`,
+    "any.required": `Поле {#label} обязательно для заполнения`,
+  }),
+  image: Joi.string().required().uri().messages({
+    "string.base": `Поле {#label} должно быть строкой`,
+    "string.empty": `Поле {#label} не может быть пустым`,
+    "any.required": `Поле {#label} обязательно для заполнения`,
+    "string.uri": `Поле {#label} должно быть валидной ссылкой`,
+  }),
+  trailerLink: Joi.string().required().uri().messages({
+    "string.base": `Поле {#label} должно быть строкой`,
+    "string.empty": `Поле {#label} не может быть пустым`,
+    "any.required": `Поле {#label} обязательно для заполнения`,
+    "string.uri": `Поле {#label} должно быть валидной ссылкой`,
+  }),
+  thumbnail: Joi.string().required().uri().messages({
+    "string.base": `Поле {#label} должно быть строкой`,
+    "string.empty": `Поле {#label} не может быть пустым`,
+    "any.required": `Поле {#label} обязательно для заполнения`,
+    "string.uri": `Поле {#label} должно быть валидной ссылкой`,
+  }),
+  movieId: Joi.number().required().messages({
+    "number.base": `Поле {#label} должно быть числом`,
+    "any.required": `Поле {#label} обязательно для заполнения`,
+  }),
+  nameRU: Joi.string().required().messages({
+    "string.base": `Поле {#label} должно быть строкой`,
+    "string.empty": `Поле {#label} не может быть пустым`,
+    "any.required": `Поле {#label} обязательно для заполнения`,
+  }),
+  nameEN: Joi.string().required().messages({
+    "string.base": `Поле {#label} должно быть строкой`,
+    "string.empty": `Поле {#label} не может быть пустым`,
+    "any.required": `Поле {#label} обязательно для заполнения`,
+  }),
+};
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+//общий метод валидации
+const movieValidation = ({ ...args }) => {
+  return celebrate({
+    body: Joi.object().keys(args),
+  });
+};
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+//метод валидации регистрации пользователя
+const movieCreateValidation = () => {
+  return movieValidation({
+    country: movieValKeys.country,
+    director: movieValKeys.director,
+    duration: movieValKeys.duration,
+    year: movieValKeys.year,
+    description: movieValKeys.description,
+    image: movieValKeys.image,
+    trailerLink: movieValKeys.trailerLink,
+    thumbnail: movieValKeys.thumbnail,
+    movieId: movieValKeys.movieId,
+    nameRU: movieValKeys.nameRU,
+    nameEN: movieValKeys.nameEN,
+  });
+};
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+const movieDeleteValidation = () => {
+  return celebrate({
+    params: Joi.object().keys({
+      movieId: Joi.string().required().hex().length(24).messages({
+        "string.length": `Поле {#label} должно быть длиною в 24 символа`,
+        "string.empty": `Поле {#label} не может быть пустым`,
+        "any.required": `Поле {#label} обязательно для заполнения`,
+        "string.hex": `Поле {#label} должно быть шестнадцатеричным`,
+      }),
+    }),
+  });
+};
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+//экспорт
+module.exports = { movieCreateValidation, movieDeleteValidation };
